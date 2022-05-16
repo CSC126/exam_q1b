f2d6e1fa67d2400d4e2e9b47875ed1d16b272cd9
Maxim Voyevoda <maximvoyevoda@gmail.com>
2022-05-16 13:11:17 -0400

    output displayed at bottom of code

--- exam_q1b/main.cpp
+++ exam_q1b/main.cpp
@@ -1,14 +1,40 @@
-//
-//  main.cpp
-//  exam_q1b
-//
-//  Created by Maxim Voyevoda on 5/16/22.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+
+    int semesterCredits[6] = { 10, 12, 16, 15, 14, 11 };
+    int i;
+    int course = 3;
+    
+    for (int i = 0; i < 6; i++){
+        
+        cout << semesterCredits[i] << " - ";
+        
+        if (semesterCredits[i] < 12){
+            
+            cout << "\nAdd for full time status!\n";
+            semesterCredits[i] += course;
+            
+        }
+        
+    }
+    
+    cout << endl;
+    
+    for (i = 5; i >=0; i--)
+        cout << semesterCredits[i] << " # ";
+    
     return 0;
 }
+
+//OUTPUT:
+
+/*
+ 10 -
+ Add for full time status!
+ 12 - 16 - 15 - 14 - 11 -
+ Add for full time status!
+
+ 14 # 14 # 15 # 16 # 12 # 13 # Program ended with exit code: 0
+ */
